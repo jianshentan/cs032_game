87f0765a6137b83365cf1c9dd7a2c107b4afbafd
Jian Shen Tan <jianshentan@mail.com>
2013-04-10 23:07:28 -0400

    pushed initial code + some fun horse assets

---
+++ src/Room.java
@@ -0,0 +1,38 @@
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.tiled.TiledMap;
+
+public class Room extends BasicGame{
+	
+	private TiledMap m_horseMap;
+	
+	public Room() {
+		super ("Room");
+		try {
+			m_horseMap = new TiledMap("assets/testmap.tmx");
+		} catch (SlickException e) {
+			System.out.println("ERROR: Could not open testmap.tmx");
+		}
+	}
+
+	@Override
+	public void render(GameContainer arg0, Graphics arg1) throws SlickException {
+		
+	}
+
+	@Override
+	public void init(GameContainer arg0) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(GameContainer arg0, int arg1) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+}
