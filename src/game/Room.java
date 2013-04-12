5a0e08e918c9d700aacfbc4efbc7cc7d92516fd5
Jian Shen Tan <jianshentan@mail.com>
2013-04-11 22:16:59 -0400

    added player class

--- src/game/Room.java
+++ src/game/Room.java
@@ -11,6 +11,7 @@ import org.newdawn.slick.tiled.TiledMap;
 public class Room extends BasicGame{
 	
 	private TiledMap m_horseMap;
+	private Player m_player;
 	private Animation m_sprite, m_up, m_down, m_left, m_right;	
 	private float m_x = 256f, m_y = 256f;
 	
@@ -25,40 +26,25 @@ public class Room extends BasicGame{
 	@Override
 	public void render(GameContainer container, Graphics g) throws SlickException {
 		m_horseMap.render(0, 0);
-		m_sprite.draw((int)m_x, (int)m_y);
+		m_sprite.draw((int)m_player.getX(), (int)m_player.getY());
 	}
 
 	@Override
 	public void init(GameContainer container) throws SlickException {
-		Image [] movementUp = {new Image("assets/Sprite1Back.png"), new Image("assets/Sprite1Back.png")};
-        Image [] movementDown = {new Image("assets/Sprite1Front.png"), new Image("assets/Sprite1Front.png")};
-        Image [] movementLeft = {new Image("assets/Sprite1Left.png"), new Image("assets/Sprite1Left.png")};
-        Image [] movementRight = {new Image("assets/Sprite1Right.png"), new Image("assets/Sprite1Right.png")};
-        int [] duration = {300, 300}; 
-         
-		try {
-			m_horseMap = new TiledMap("assets/testmap.tmx");
-		} catch (SlickException e) {
-			System.out.println("ERROR: Could not open testmap.tmx");
-		}
+		// setup player
+		m_player = new Player(m_x, m_y);
 		
-        /*
-         * false variable means do not auto update the animation.
-         * By setting it to false animation will update only when
-         * the user presses a key.
-         */
-        m_up = new Animation(movementUp, duration, false);
-        m_down = new Animation(movementDown, duration, false);
-        m_left = new Animation(movementLeft, duration, false);
-        m_right = new Animation(movementRight, duration, false);
-        
-        // Original orientation of the sprite. It will look right.
-        m_sprite = m_right;
+		try {
+			m_horseMap = new TiledMap("assets/10X10.tmx");
+		} catch (SlickException e) {
+			System.out.println("ERROR: Could not 10X10.tmx");
+		}
         
         // build a collision map based on tile properties in the TileD map
         m_blocked = new boolean[m_horseMap.getWidth()][m_horseMap.getHeight()];
+        System.out.println("width: " + m_horseMap.getWidth());
 
-       for (int xAxis=0; xAxis<m_horseMap.getWidth(); xAxis++) {
+        for (int xAxis=0; xAxis<m_horseMap.getWidth(); xAxis++) {
             for (int yAxis=0; yAxis<m_horseMap.getHeight(); yAxis++) {
                 int tileID = m_horseMap.getTileId(xAxis, yAxis, 0);
                 String value = m_horseMap.getTileProperty(tileID, "blocked", "false");
@@ -72,12 +58,12 @@ public class Room extends BasicGame{
 
 	@Override
 	public void update(GameContainer container, int delta) throws SlickException {
+    // The lower the delta the slowest the sprite will animate.
 		Input input = container.getInput();
         if (input.isKeyDown(Input.KEY_UP)) {
             m_sprite = m_up;
             if (!isBlocked(m_x, m_y - delta * 0.1f)) {
                 m_sprite.update(delta);
-                // The lower the delta the slowest the sprite will animate.
                 m_y -= delta * 0.1f;
             }
         }
