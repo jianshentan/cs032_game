d1ef7d5143e10b2505b35f4bb23e317093a8b8e9
Zachary Salmon <zachary_salmon@brown.edu>
2013-05-02 15:58:30 -0400

    night time stuff finished, altho could still use polish

--- src/game/Spectre.java
+++ src/game/Spectre.java
@@ -3,6 +3,7 @@ package game;
 import org.newdawn.slick.Animation;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.SpriteSheet;
 
 import game.gameplayStates.GamePlayState;
 import game.player.Player;
@@ -17,11 +18,12 @@ public class Spectre extends Enemy {
 			System.out.println("ERROR: spectre has wrong # of destinations");
 		}
 		//set sprites
+		SpriteSheet spectreSprite = new SpriteSheet("assets/humanoid_standing.png", 64, 64);
 		this.setLeadTo(destinations[3-StateManager.m_dreamState][0], destinations[3-StateManager.m_dreamState][1]);
-		Image [] movementUp = {new Image("assets/Sprite1Back.png"), new Image("assets/Sprite1Back.png")};
-        Image [] movementDown = {new Image("assets/Sprite1Front.png"), new Image("assets/Sprite1Front.png")};
-        Image [] movementLeft = {new Image("assets/Sprite1Left.png"), new Image("assets/Sprite1Left.png")};
-        Image [] movementRight = {new Image("assets/Sprite1Right.png"), new Image("assets/Sprite1Right.png")};
+		Image [] movementUp = {spectreSprite.getSprite(1,0), spectreSprite.getSprite(1,0)};
+        Image [] movementDown = {spectreSprite.getSprite(0,0), spectreSprite.getSprite(0,0)};
+        Image [] movementLeft = {spectreSprite.getSprite(3,0), spectreSprite.getSprite(3,0)};
+        Image [] movementRight = {spectreSprite.getSprite(2,0), spectreSprite.getSprite(2,0)};
         int [] duration = {300, 300}; 
         
         //turn sprites into animations
