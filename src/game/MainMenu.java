cc01ef36a2ddf3bec0514d8fda1bf34b796120c1
Annalia Sunderland <annaliasunderland@gmail.com>
2013-05-09 00:43:05 -0400

    main menu!

--- src/game/MainMenu.java
+++ src/game/MainMenu.java
@@ -7,6 +7,7 @@ import java.io.FileNotFoundException;
 
 import javax.xml.stream.XMLStreamException;
 
+import org.newdawn.slick.Animation;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
@@ -14,6 +15,7 @@ import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.ShapeFill;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.SpriteSheet;
 import org.newdawn.slick.fills.GradientFill;
 import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.state.BasicGameState;
@@ -31,6 +33,9 @@ public class MainMenu extends BasicGameState {
  	private Rectangle m_rectangle;
 	private ShapeFill m_shapeFill;
 	
+	private int m_mancounter = 0;
+	private Animation m_animatedman;
+	
 	private int m_selection = 0;
 	private int m_inputDelta = 0;
 	
@@ -45,6 +50,18 @@ public class MainMenu extends BasicGameState {
 		m_rectangle = new Rectangle(m_x, m_y, m_width, m_height);
 		m_shapeFill = new GradientFill(m_x, m_y, Color.white,
 									   m_x + m_width, m_y + m_height, Color.white);
+		
+		try {
+			SpriteSheet spritefile = new SpriteSheet("assets/characters/player.png",64,64);
+			Image [] image_man = new Image[] {spritefile.getSprite(0, 0),spritefile.getSprite(3,0),spritefile.getSprite(1,0),
+					spritefile.getSprite(2,0)};
+			int [] durations = {10,10,10,10};
+			this.m_animatedman = new Animation(image_man,durations,false);
+			
+		} catch (SlickException e) {
+			
+		}
+		
 	}
 	
 	@Override
@@ -76,12 +93,27 @@ public class MainMenu extends BasicGameState {
 			g.drawString("<", m_width/2 + 35, m_height/2 + 105);
 			break;
 		}
+		
+		this.m_animatedman.draw(m_width/2 - 32, m_height/2 - 120);
+		
 	}
 
 	@Override
 	public void update(GameContainer container, StateBasedGame stateManager, int delta)
 			throws SlickException {
+        this.m_mancounter++;
         
+        if (m_mancounter % 150 == 0) {
+        	int frame = m_animatedman.getFrame();
+            if (frame == 3) {
+            	frame = -1;
+            }
+            m_animatedman.setCurrentFrame(frame + 1);
+        }
+        
+		
+		
+		
 		Input input = container.getInput();
 		m_inputDelta-=delta;
         if (m_inputDelta<0 && input.isKeyDown(Input.KEY_UP)) {
